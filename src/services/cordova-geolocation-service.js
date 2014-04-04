9dd271bcb51889193d753845921ceef6d5711547
Robin van Baalen <robin@stylr.nl>
2014-04-03 20:51:56 -0700

    Add watchCurrentPosition to GeolocationService
    Added the watchCurrentPosition using a deferred's `notify` method.
    
    Usage:
    
    ```
    GeolocationService.watchCurrentPosition()
       .then(null,null,function (geolocationData) {
           var position = geolocationData.position;
           
           if (!withinBounds(position)) {
               GeolocationService.stopWatcher();
           }
       });
    ```

--- src/services/cordova-geolocation-service.js
+++ src/services/cordova-geolocation-service.js
@@ -1,6 +1,7 @@
 angular.module('cordova.services')
 .service("GeolocationService", ['$q', '$log', function ($q, $log) {
-
+    var watcher;
+    
     var service = {
         promiseCurrentPosition: function (geolocationOptions) {
             var deferred = $q.defer();
@@ -20,7 +21,32 @@ angular.module('cordova.services')
             $log.debug("GeolocationService: return promise" );
             return deferred.promise;
         },
-        geolocation: navigator.geolocation
+        geolocation: navigator.geolocation,
+        watchCurrentPosition: function (geolocationOptions) {
+            var deferred = $q.defer();
+            
+            $log.debug("GeolocationService: promiseCurrentPosition" );
+            watcher = navigator.geolocation.watchCurrentPosition(
+                function (position) {
+                    $log.debug("GeolocationService: success" );
+                    deferred.notify({position: position, watcher: watcher});
+                },
+                function (error) {
+                    $log.debug("GeolocationService: error" );
+                    deferred.reject({error: error, watcher: watcher});
+                },
+                geolocationOptions
+            );
+            
+            $log.debug("GeolocationService: return promise" );
+            //*** This promise is never resolved since watch will always stay alive.
+            return deferred.promise;
+        },
+        stopWatcher: function () {
+            if (watcher) {
+                navigator.geolocation.clearWatch(watcher);
+            }
+        }
     }
 
     return service;
